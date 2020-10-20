1bdf0acd33aa5b8cbd2102105e090e13c8a01061
Philipp Krüger <philipp.krueger1@gmail.com>
2020-10-20 15:31:20 +0200

    :sparkles: Prettyprint the figma json after fetching

--- fetch-figma.ts
+++ fetch-figma.ts
@@ -1,10 +1,17 @@
 import fetch from "node-fetch";
 import * as fs from "fs/promises";
-import { config } from "dotenv";
+import * as dotenv from "dotenv";
 
-config();
+dotenv.config();
 
 const { FIGMA_TOKEN } = process.env;
+
+if (FIGMA_TOKEN == null) {
+    throw "Missing environment parameter: FIGMA_TOKEN\nFind out how to get your API token here: https://www.figma.com/developers/api#access-tokens";
+}
+
+fetchFigma(FIGMA_TOKEN);
+
 
 async function fetchFigma(apiKey: string) {
     const figmaJson = await fetch("https://api.figma.com/v1/files/k7bHLlSMVnTWzClpRHLx6a", {
@@ -12,8 +19,8 @@ async function fetchFigma(apiKey: string) {
             "X-Figma-Token": apiKey,
         },
     });
-    
-    await fs.writeFile("test/elm-figma-autoflex-test.json", figmaJson.body.read());
-}
 
-fetchFigma(FIGMA_TOKEN);
+    const json = JSON.parse(figmaJson.body.read().toString());
+    const pretty = JSON.stringify(json, null, 4);
+    await fs.writeFile("test/elm-figma-autoflex-test.json", pretty);
+}
